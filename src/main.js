dc3a6144eb3c8e963b2525323632927efca63572
dsibi <dasibirin@gmail.com>
2023-07-06 23:12:24 +0300

    add partials

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
+import header from "./components/header/header.hbs";
 import main from "./pages/main/main.hbs";
-// import chatPic from '../../../static/chatPic.svg'
-// import './login.less'
+import logoPath from "../static/img/logo.png";
+import "./default.css";
 
-function returnMain() {
-  return main();
-}
+import Handlebars from "handlebars";
+Handlebars.registerPartial("header", header);
 
-const root = document.getElementById("app");
-root.innerHTML = returnMain();
+const mn = main({ logoPath: logoPath });
+// const mn = main();
+// const root = document.body;
+// const result = hd;
+// console.log(hd);
+// root.innerHTML = result;
+
+// document.body.prepend(hd);
+
+const root2 = document.getElementById("app");
+// console.log(root);
+// console.log(root2);
+const result2 = mn;
+root2.innerHTML = result2;
+
+// var tag = document.createElement("p");
+// // console.log(document);
+// var text = document.createTextNode("Tutorix is the best e-learning platform");
+// tag.appendChild(text);
+// var element = document.getElementById("app");
+// element.appendChild(tag);
