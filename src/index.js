f9bd1b2ea1af6bdbfe2e6a4b5b116ad1e2e88c14
dsibi <dasibirin@gmail.com>
2023-07-07 12:08:30 +0300

    index.js init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import logoPath from "../static/img/logo.png";
+import mainPage from "./pages/main/main";
+
+document.querySelector('[rel="icon"]').setAttribute("href", logoPath);
+
+mainPage();
---
+++ src/pages/main/main.js
@@ -0,0 +1,17 @@
+import iMain from "../../pages/main/main.hbs";
+import "../../pages/main/main.css";
+import iHeader from "../../components/header/header.hbs";
+import iButton from "../../components/button/button.hbs";
+import logoPath from "../../../static/img/logo.png";
+import "../../default.css";
+import Handlebars from "handlebars";
+
+Handlebars.registerPartial("header", iHeader);
+Handlebars.registerPartial("button", iButton);
+
+export default function mainPage() {
+  const main = iMain({ logoPath: logoPath });
+  const root = document.getElementById("app");
+  const result = main;
+  root.innerHTML = result;
+}
