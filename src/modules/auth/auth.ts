046603089a2a9be91a4780092bd52c7d02e96ecf
dsibi <dasibirin@gmail.com>
2023-07-07 17:35:54 +0300

    tsconfig edit

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -1,6 +1,5 @@
 import { overOutField, onClickField } from "./inputFields";
 import "./auth.css";
-// import "../../default.css";
 
 let lgField: HTMLElement = document.getElementById("login")!;
 let pwField: HTMLElement = document.getElementById("password")!;
