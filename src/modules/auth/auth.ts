b2178149d49b6ac7d7ac150272e6376207ab858e
dsibi <dasibirin@gmail.com>
2023-07-12 10:21:46 +0300

    auth fixed

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/semi */
-// import { overOutField, onClickField } from "./inputFields";
+import { overOutField, onClickField } from "./inputFields";
 import "./auth.css";
 import iHeader from "../../components/header/header.hbs";
 import iButton from "../../components/button.hbs";
@@ -9,18 +9,16 @@ import Handlebars from "handlebars";
 Handlebars.registerPartial("header", iHeader);
 Handlebars.registerPartial("button", iButton);
 
-// const lgField: HTMLElement | boolean =
-//   document.getElementById("login") ?? false;
-// const pwField: HTMLElement | boolean =
-//   document.getElementById("password") ?? false;
-
-// overOutField(lgField as HTMLSelectElement);
-// onClickField(lgField as HTMLSelectElement);
-
-// overOutField(pwField as HTMLSelectElement);
-
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("app") as HTMLInputElement;
-  const result = auth({ value: "DDDDdsfsd" });
+  const result = auth();
   root.innerHTML = result;
+
+  const lgField: HTMLElement | null = document.getElementById("login");
+  const pwField: HTMLElement | null = document.getElementById("password");
+
+  overOutField(lgField as HTMLSelectElement);
+  onClickField(lgField as HTMLSelectElement);
+
+  overOutField(pwField as HTMLSelectElement);
 });
