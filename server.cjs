21e7268ffaa85cd219da7a96abbb6114d53c1eda
dsibi <dasibirin@gmail.com>
2023-07-26 20:54:46 +0300

    changes in server

--- server.cjs
+++ server.cjs
@@ -1,18 +1,3 @@
-// // import express from "express";
-
-// const express = require("express");
-
-// const app = express();
-// const PORT = 3000;
-
-// // app.use(express.static("./dist/"));
-// app.use(express.static("public"));
-// app.use("/static", express.static("static"));
-
-// app.listen(PORT, function () {
-//   console.log(`My chat app started on port ${PORT}!`);
-// });
-
 // Requiring module
 const express = require("express");
 
