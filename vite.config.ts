c5b85000475e9ec2c423d9c3c5eef265dc71da3b
dsibi <dasibirin@gmail.com>
2023-07-27 09:46:12 +0300

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import handlebars from "./vite-plugin-handlebars-precompile";
 
 export default defineConfig({
   build: {
-    outDir: resolve("dist"),
+    outDir: "dist",
   },
-  publicDir: "/static",
+  publicDir: "static",
   plugins: [handlebars()],
 });
