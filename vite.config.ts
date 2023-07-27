4217cc491bfb68894edae9961d1474d06b467f7d
dsibi <dasibirin@gmail.com>
2023-07-27 09:49:25 +0300

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import handlebars from "./vite-plugin-handlebars-precompile";
 
 export default defineConfig({
+  root: "./",
   build: {
     outDir: "dist",
   },
