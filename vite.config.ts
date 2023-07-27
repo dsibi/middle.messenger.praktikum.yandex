07a2f13441440ebcd843595b47fde468b6f622a1
dsibi <dasibirin@gmail.com>
2023-07-27 09:58:06 +0300

    index moved inside src

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import handlebars from "./vite-plugin-handlebars-precompile";
 
 export default defineConfig({
-  root: "./",
+  root: "src",
   build: {
     outDir: "dist",
   },
